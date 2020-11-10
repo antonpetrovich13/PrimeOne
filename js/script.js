5310c5b5463620279a841a05a180c6c4416c87d4
Anton Petrovich <63902821+antonpetrovich13@users.noreply.github.com>
2020-11-10 22:49:19 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -73,3 +73,28 @@ $(function() {
    });
 });
 
+// СЛАЙДЕР
+$(window).on('load resize', function() {
+	if ($(window).width() < 950) {
+	$('#slider:not(.slick-initialized)').slick({
+		dots: true,
+		arrows: false,
+		infinite: true,
+		speed: 500,
+		cssEase: 'ease-out',
+		touchThreshold:10,
+		adaptiveHeight: true,
+		slidesToShow: 2,
+		responsive:[
+			{
+				breakpoint: 650,
+				settings: {
+					slidesToShow: 1,
+				}
+			}
+		]
+		});
+	} else {
+		$("#slider.slick-initialized").slick("unslick");
+	}
+});
